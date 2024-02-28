00ed8831b0624c02836033bb5fdb16c94bd0d27b
Seo-BJ <binijuni00@gmail.com>
2024-02-28 15:58:39 +0900

    Make Character, Enhanced Input, Player Controller

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,24 @@
+
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = true;
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), "WeaponHandSocket");
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
